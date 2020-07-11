811470d71e080e7d45b4558c7911534bf9164b9b
kamiprok <kamiprok@gmail.com>
2020-07-12 01:52:38 +0200

    rewrite

--- kroos.py
+++ kroos.py
@@ -1,12 +1,12 @@
 import discord
 from datetime import datetime
-import asyncio
+from discord.ext import commands
+from discord.ext.tasks import loop
 import os
 
 TOKEN = os.environ['token']
 
-client = discord.Client()
-# id = 135799278336475136
+bot = commands.Bot(command_prefix='/')
 
 
 async def clock():
@@ -18,69 +18,81 @@ async def clock():
     return clock.now, clock.day, clock.today, clock.time, clock.longdate
 
 
-@client.event
+@loop(seconds=5)
+async def change_status():
+    await bot.wait_until_ready()
+    while True:
+        await clock()
+        bot.activity = discord.Game(name=f'{clock.time} {clock.day}, {clock.today}')
+        await bot.change_presence(status=discord.Status.online, activity=bot.activity)
+
+
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
-    print(f'Client ID = {client.user.id}')
+    print(f"We have logged in as {bot.user}")
+    print(f'Client ID = {bot.user.id}')
     print(f'Discord version = {discord.__version__}')
-    print(f'Server name = {client.get_guild(135799278336475136)}')
-    print(f'Users = {client.get_guild(135799278336475136).member_count}')
-    channel = client.get_channel(705808157863313468)
+    print(f'Server name = {bot.get_guild(135799278336475136)}')
+    print(f'Users = {bot.get_guild(135799278336475136).member_count}')
+    channel = bot.get_channel(705808157863313468)
+    print(f'We are in {channel}')
     await channel.send("I'm Online! Type help for all commands.")
 
 
-async def change_status():
-    await client.wait_until_ready()
-    while True:
-        await clock()
-        client.activity = discord.Game(name=f'{clock.time} {clock.day}, {clock.today}')
-        await client.change_presence(status=discord.Status.online, activity=client.activity)
-        await asyncio.sleep(5)
-
-
-@client.event
+@bot.event
 async def on_member_join(member):
     print(f'{member.name} has joined')
-    await client.get_channel(705808157863313468).send(f'Welcome to Miami Nights, {member.mention}')
+    await bot.get_channel(705808157863313468).send(f'Welcome to Miami Nights, {member.mention}')
 
 
-@client.event
+@bot.event
 async def on_message(message):
     await clock()
     print(f'{clock.time} {message.author.display_name}({message.author}): {message.content}')
-    if message.author == client.user:
-        return
-
-    if message.content.lower() == 'hello':
-        await message.channel.send(f'Hello, {message.author.display_name}!')
-
-    if message.content.lower() == 'ping':
-        x = f'{client.latency:.3f}'
-        x = float(x)*1000
-        x = int(x)
-        x = f'{x} ms'
-        await message.channel.send(f'pong, {x}')
-
-    if message.content.lower() == 'time':
-        await clock()
-        await message.channel.send(f"It's {clock.time}. Today is {clock.longdate}")
-
-    if message.content.lower() == 'help':
-        await message.channel.send('```list of commands:\n'
-                                   'help - displays this list\n'
-                                   'time - displays current time\n'
-                                   'ping - displays delay between your pc and discord server\n'
-                                   'hello - greets user\n'
-                                   'img - displays kroos.img\n'
-                                   'Bot is still in developement. More functions to come soon!```')
 
     for x in message.mentions:
-        if x == client.user:
+        if x == bot.user:
             await message.channel.send(f'What do you want, {message.author.mention}?')
 
-    if message.content.lower() == 'img':
-        await message.channel.send(file=discord.File('kroos.jpg'))
+    await bot.process_commands(message)
 
 
-client.loop.create_task(change_status())
-client.run(TOKEN)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hello there, {ctx.author.mention}!')
+
+
+@bot.command()
+async def ping(ctx):
+    print(bot.latency)
+    x = f'{bot.latency:.3f}'
+    x = float(x) * 1000
+    x = int(x)
+    x = f'{x} ms'
+    await ctx.send(f'pong, {x}')
+
+
+@bot.command()
+async def time(ctx):
+    await clock()
+    await ctx.send(f"It's {clock.time}. Today is {clock.longdate}")
+
+
+@bot.command()
+async def img(ctx):
+    await ctx.send(file=discord.File('kroos.jpg'))
+
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send('```list of commands:\n'
+                               '/commands - displays this list\n'
+                               '/time - displays current time\n'
+                               '/ping - displays delay between your pc and discord server\n'
+                               '/hello - greets user\n'
+                               '/img - displays kroos.img\n'
+                               'Bot is still in developement. More functions to come soon!```')
+
+
+change_status.start()
+bot.run(TOKEN)
